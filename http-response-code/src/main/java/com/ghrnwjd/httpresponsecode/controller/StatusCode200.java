fdea97bea1a6d2cbfff16ac3282e79aedf9862f5
ghrnwjd <hoyoung.wjd@gmail.com>
2024-01-16 16:11:43 +0900

    feat: HTTP Status 200

---
+++ http-response-code/src/main/java/com/ghrnwjd/httpresponsecode/controller/StatusCode200.java
@@ -0,0 +1,48 @@
+package com.ghrnwjd.httpresponsecode.controller;
+
+import com.ghrnwjd.httpresponsecode.data.dto.PersonDTO;
+import com.ghrnwjd.httpresponsecode.data.dto.ResponseDTO;
+import com.ghrnwjd.httpresponsecode.data.entity.Person;
+import com.ghrnwjd.httpresponsecode.service.PersonService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/response")
+public class StatusCode200 {
+
+    @Autowired
+    private PersonService personService;
+    @GetMapping("/200")
+    public ResponseDTO success() {
+        return ResponseDTO.builder()
+                .code(HttpStatus.OK.value())
+                .response(HttpStatus.OK)
+                .comment("요청 성공.")
+                .build();
+    }
+
+    @PostMapping("/201")
+    public ResponseDTO<Person> created(@RequestBody PersonDTO dto) {
+        Person person = personService.save(dto);
+        log.info(person.toString());
+
+        return new ResponseDTO<Person>(person, HttpStatus.ACCEPTED.value(),
+                HttpStatus.ACCEPTED, "요청이 정상적이며 새로운 리소스가 생겼습니다.");
+    }
+
+    @PostMapping("/204")
+    public ResponseDTO noContent(@RequestBody PersonDTO dto) {
+        log.info(dto.toString());
+        return ResponseDTO.builder()
+                .code(HttpStatus.NO_CONTENT.value())
+                .response(HttpStatus.NO_CONTENT)
+                .comment("요청은 정상적이나 응답할 데이터가 없습니다.")
+                .build();
+    }
+
+}
